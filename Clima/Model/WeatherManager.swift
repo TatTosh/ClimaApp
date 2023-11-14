4c158ba0ec5955a0d802439aad08ad1871264d8c
Tatjana Urvan <tatjana.urvan@nl.abnamro.com>
2023-11-14 17:02:11 +0100

    Add json decoding files and functions

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -21,20 +21,28 @@ struct WeatherManager {
 			
 			let session = URLSession(configuration: .default)
 			
-			let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+			let task = session.dataTask(with: url) { data, response, error in
+				if error != nil {
+					print(error!)
+					return
+				}
+				
+				if let safeData = data {
+					parseJson(weatherData: safeData)
+				}
+			}
 			
 			task.resume()
 		}
 	}
 	
-	func handle(data: Data?, response: URLResponse?, error: Error?) {
-		if error != nil {
-			print(error!)
-			return
-		}
-		
-		if let safeData = data {
-			let dataString = String(data: safeData, encoding: .utf8)
+	func parseJson(weatherData: Data) {
+		let decoder = JSONDecoder()
+		do {
+			let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+			print(decodedData.weather[0].description)
+		} catch {
+			print(error)
 		}
 	}
 }
